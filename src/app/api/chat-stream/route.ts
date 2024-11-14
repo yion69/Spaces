9aad2c44b01a5932c5ceb2cee2e8d7e106110282
Thu Ta Naing <thutanaing.personal@gmail.com>
2024-11-14 12:18:25 +0630

    chatbot api fixes

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -1,15 +1,27 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 export const POST = async (req:NextRequest) => {
-    const { message } = await req.json();
+    
+    const { message, session_id } = await req.json();
 
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    try {
+    
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+        const prompt = message;
+    
+        const result = await model.generateContent(prompt);
+    
+        return NextResponse.json({ session: session_id, message: message});
 
-    const prompt = message;
+    } catch (error) {
+        console.error("ERROR:",error);
 
-    const result = await model.generateContent(prompt);
-
-    return result;
-}
+        return NextResponse.json(
+            { session: session_id, error: error || "An unknown error occurred"},
+            { status: 500}
+        )
+    }
+}  
